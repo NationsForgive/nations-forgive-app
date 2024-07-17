f7e70714c454d49ad2145dff0dd47b82aced18e2
José Padrón <43755294+joselpadronc@users.noreply.github.com>
2024-07-16 21:03:21 -0400

    fix: correction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,10 +24,10 @@ export default function Home() {
               style={{ borderRadius: 8 }}
               src="https://www.youtube.com/embed/cQa3NBmYGnc?si=HX0PPJ1C5uZLtNjb"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           </Grid>
           <Grid
@@ -98,10 +98,10 @@ export default function Home() {
               style={{ borderRadius: 8 }}
               src="https://www.youtube.com/embed/cQa3NBmYGnc?si=HX0PPJ1C5uZLtNjb"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           </Grid>
         </Grid>
